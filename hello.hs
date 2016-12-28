2d87d84d0316cb9a4197cec2aaad654ee37d4ada
Amar Shah <amar47shah@gmail.com>
2016-12-28 17:26:05 -0500

    Interview pairing session

--- hello.hs
+++ hello.hs
@@ -1,3 +1,19 @@
+{-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE OverloadedStrings #-}
 import Reflex.Dom
-main = mainWidget $ text "Hello, world!"
+import Data.Monoid ((<>))
+import Data.Text (Text)
+
+main = mainWidget $ el "div" $ do
+  rec text <- textInput $
+        def { _textInputConfig_setValue = const "" <$> submit }
+      submit <- button "submit"
+  clear <- button "clear"
+  let submitEvent = tagPromptlyDyn (Just <$> value text) submit
+  let clearEvent = const Nothing <$> clear
+  sentence <- foldDyn toSentence "" $ leftmost [submitEvent, clearEvent]
+  dynText sentence
+
+toSentence :: Maybe Text -> Text -> Text
+toSentence mw s =
+  maybe "" (\w -> s <> " " <> w) mw
